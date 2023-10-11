440a3ca22ec11e9d6a83b1054f88257f179ee8a9
Matheus Silva <98681828+Tetheugas@users.noreply.github.com>
2023-10-11 18:36:47 -0300

    Register of Jobs opportunities
    The CRUD has started ✓

--- src/main/java/com/AppRH/AppRH/controllers/VagaController.java
+++ src/main/java/com/AppRH/AppRH/controllers/VagaController.java
@@ -1,11 +1,15 @@
 package com.AppRH.AppRH.controllers;
 
 
+import com.AppRH.AppRH.models.Vaga;
 import com.AppRH.AppRH.repository.CandidateRepository;
 import com.AppRH.AppRH.repository.VagaRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class VagaController {
@@ -17,6 +21,19 @@ public class VagaController {
     @RequestMapping(value = "/cadastrarVaga", method = RequestMethod.GET)
     public String form() {
         return "vaga/formVaga";
+    }
+
+    @RequestMapping(value = "/cadastrarVaga", method = RequestMethod.POST)
+    public String form(@Valid Vaga vaga, BindingResult result, RedirectAttributes attributes) {
+
+        if(result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique os campos...");
+            return "redirect:/cadastrarVaga";
+        }
+
+        vr.save(vaga);
+        attributes.addFlashAttribute("mensagem", "Vaga cadastrada com sucesso!");
+        return "redirect:/cadastrarVaga";
     }
 
 }
