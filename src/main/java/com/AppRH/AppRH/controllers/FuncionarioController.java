d060ebbca1a45011cfdaefa7cfb2805a0ae754fe
Matheus Silva <98681828+Tetheugas@users.noreply.github.com>
2023-10-25 09:39:42 -0300

    Update funcionarioController & Repository

--- src/main/java/com/AppRH/AppRH/controllers/FuncionarioController.java
+++ src/main/java/com/AppRH/AppRH/controllers/FuncionarioController.java
@@ -1,9 +1,17 @@
 package com.AppRH.AppRH.controllers;
 
+import com.AppRH.AppRH.models.Funcionario;
 import com.AppRH.AppRH.repository.DependentesRepository;
 import com.AppRH.AppRH.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
 
 @Controller
 public class FuncionarioController {
@@ -13,5 +21,24 @@ public class FuncionarioController {
 
     @Autowired
     private DependentesRepository dr;
+
+    // chama o forms de cadastrar os funcionarios
+    @RequestMapping(value = "/cadastrarFuncionario", method = RequestMethod.GET)
+    public String form() {
+        return "funcionario/formFuncionario";
+    }
+    // cadastrar os funcionarios
+    @RequestMapping(value = "/cadastrarFuncionario", method = RequestMethod.POST)
+    public String form(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attributes) {
+
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique os campos");
+            return "redirect:/cadastrarFuncionario";
+        }
+
+        fr.save(funcionario);
+        attributes.addFlashAttribute("mensagem","Funcionário cadastrado com sucesso!");
+        return "redirect:/cadastrarFuncionario";
+    }
 
 }
