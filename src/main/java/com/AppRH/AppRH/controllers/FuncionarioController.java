d0299643e44333cb3c8156774c37db3addf6ce06
Matheus Silva <98681828+Tetheugas@users.noreply.github.com>
2023-10-25 10:02:44 -0300

    Update FuncionarioController & DependentesRepository

--- src/main/java/com/AppRH/AppRH/controllers/FuncionarioController.java
+++ src/main/java/com/AppRH/AppRH/controllers/FuncionarioController.java
@@ -1,13 +1,16 @@
 package com.AppRH.AppRH.controllers;
 
+import com.AppRH.AppRH.models.Dependentes;
 import com.AppRH.AppRH.models.Funcionario;
 import com.AppRH.AppRH.repository.DependentesRepository;
 import com.AppRH.AppRH.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.naming.Binding;
@@ -39,6 +42,31 @@ public class FuncionarioController {
         fr.save(funcionario);
         attributes.addFlashAttribute("mensagem","Funcionário cadastrado com sucesso!");
         return "redirect:/cadastrarFuncionario";
+
+    }
+
+    // list funcionario
+    @RequestMapping("/funcionarios")
+    public ModelAndView listaFuncionarios() {
+        ModelAndView mv = new ModelAndView("funcionario/listaFuncionario");
+        Iterable<Funcionario> funcionarios = fr.findAll();
+        mv.addObject("funcionarios", funcionarios);
+        return mv;
+    }
+
+    // list dependentes
+    @RequestMapping(value = "/dependentes/{id}", method = RequestMethod.GET)
+    public ModelAndView dependentes(@PathVariable("id") long id) {
+        Funcionario funcionario = fr.findById(id);
+        ModelAndView mv = new ModelAndView("funcionario/dependentes");
+        mv.addObject("funcionario", funcionario);
+
+        // lista de dependentes baseada no funcionario
+        Iterable<Dependentes> dependentes = dr.findByFuncionario(funcionario);
+        mv.addObject("dependentes", dependentes);
+
+        return mv;
+
     }
 
 }
